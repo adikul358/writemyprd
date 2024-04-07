d27067bba17d3ccbed9c2c6d81db9f451bab3956
Aditya Kulshrestha <adi.kul358@gmail.com>
2024-04-07 23:40:04 +0530

    Init

---
+++ app.py
@@ -0,0 +1,21 @@
+from openai_funcs import get_prd
+
+
+def boldtext(text):
+	return f"\033[1m{text}\033[0;0m"
+
+print(f"\n\n{boldtext("1. About Your Product")}")
+a1 = input("\nProduct Name\n> ")
+a2 = input("\nFeature Name\n> ")
+a3 = input("\nGive an Overview/Explanation\n> ")
+
+print(f"\n\n{boldtext("2. Define Your Requirement")}")
+b1 = input("\nFeature List\n> ")
+b2 = input("\nUser Feedback\n> ")
+
+query = f"Write a PRD for a product named {a1} that {a3} with features including {a2}, {b1}{", for which the users 	have given feedback of" + b2 if b2 != "" else ""}"
+print(f"\n\n{boldtext("Query")}\n>{query}")
+
+prd = get_prd(query)
+print("\n\n")
+print(prd)
---
+++ openai_funcs.py
@@ -0,0 +1,52 @@
+from openai import OpenAI
+from time import sleep
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+client = OpenAI(api_key=config["OPENAI_API_KEY"])
+my_assistant = client.beta.assistants.retrieve(config["OPENAI_ASSISTANT_ID"])
+
+
+def create_thread():
+    thread = client.beta.threads.create()
+
+    return thread
+
+
+def send_message(thread_id, message):
+    thread_message = client.beta.threads.messages.create(
+        thread_id,
+        role="user",
+        content=message,
+    )
+    return thread_message
+
+
+def run_assistant(thread_id, assistant_id):
+    run = client.beta.threads.runs.create(
+        thread_id=thread_id, assistant_id=assistant_id
+    )
+    return run
+
+
+def get_newest_message(thread_id):
+    thread_messages = client.beta.threads.messages.list(thread_id)
+    return thread_messages.data[0]
+
+
+def get_run_status(thread_id, run_id):
+    run = client.beta.threads.runs.retrieve(thread_id=thread_id, run_id=run_id)
+    return run.status
+
+
+def get_prd(query):
+    my_thread = create_thread()
+
+    send_message(my_thread.id, query)
+    run = run_assistant(my_thread.id, my_assistant.id)
+    while run.status != "completed":
+        run.status = get_run_status(my_thread.id, run.id)
+        sleep(1)
+    sleep(0.5)
+    response = get_newest_message(my_thread.id)
+    return response.content[0].text.value
