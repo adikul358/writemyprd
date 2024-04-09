0a2d6cc64457c0e52ac29c9c795d055996362a85
Aditya Kulshrestha <adi.kul358@gmail.com>
2024-04-09 16:56:24 +0530

    Frontend Functionality Done

--- app.py
+++ app.py
@@ -1,21 +1,34 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf import Form
+from wtforms import StringField, TextAreaField
 from openai_funcs import get_prd
 
+app = Flask(__name__)
 
-def boldtext(text):
-	return f"\033[1m{text}\033[0;0m"
+class PRDForm(Form):
+    product_name = StringField('Product Name')
+    feature_name = StringField('Feature Name')
+    overview = TextAreaField('Give an Overview / Explanation')
+    feature_list = TextAreaField('Feature List')
+    user_feedback = TextAreaField('User Feedback')
 
-print(f"\n\n{boldtext("1. About Your Product")}")
-a1 = input("\nProduct Name\n> ")
-a2 = input("\nFeature Name\n> ")
-a3 = input("\nGive an Overview/Explanation\n> ")
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    error = ""
+    prd = ""
+    form = PRDForm(request.form)
 
-print(f"\n\n{boldtext("2. Define Your Requirement")}")
-b1 = input("\nFeature List\n> ")
-b2 = input("\nUser Feedback\n> ")
+    if request.method == 'POST':
+        a1 = form.product_name.data
+        a2 = form.feature_name.data
+        a3 = form.overview.data
+        b1 = form.feature_list.data
+        b2 = form.user_feedback.data
 
-query = f"Write a PRD for a product named {a1} that {a3} with features including {a2}, {b1}{", for which the users 	have given feedback of" + b2 if b2 != "" else ""}"
-print(f"\n\n{boldtext("Query")}\n>{query}")
+        query = f"Write a PRD for a product named {a1} that {a3} with features including {a2}, {b1}{", for which the users 	have given feedback of" + b2 if b2 != "" else ""}"
+        prd = get_prd(query)
 
-prd = get_prd(query)
-print("\n\n")
-print(prd)
+    return render_template('index.html', form=form, message=error, prd=prd)
+
+if __name__ == "__main__":
+    app.run(debug=True)
